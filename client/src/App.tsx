905241113861614b61d468e337bc8744e140f37e
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-11 03:50:40 +0300

    Changed impots in files, added paths in tsconfig.json file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header/Header";
-import {Filters} from "./components/Filters/Filters";
+import {Header, Filters} from "components";
 
 function App() {
   return (
