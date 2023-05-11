eb0bfb385102b68954f791cf4cb17db3f5a1c634
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-11 21:12:19 +0300

    New useCatalogues hook using react-query and SuperjobService

--- client/src/components/Filters/Filters.tsx
+++ client/src/components/Filters/Filters.tsx
@@ -5,17 +5,20 @@ import {NumberInput, Select, Button} from '@mantine/core';
 import {IconChevronDown} from '@tabler/icons-react';
 import {useForm} from '@mantine/form';
 
+import {useCatalogues} from "hooks";
+
+
 export function Filters() {
 
-    const filters_form = useForm({
-        initialValues: {
-            catalogue_id: '',
-            payment_from: '',
-            payment_to: ''
-        }
-    });
+    const filters_form = useForm(
+        {initialValues: {
+                catalogue_id: '',
+                payment_from: '',
+                payment_to: ''
+            }
+        });
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
         event.preventDefault();
     }
 
@@ -23,11 +26,7 @@ export function Filters() {
         filters_form.reset();
     }
 
-    const options = [
-        { value: '1', label: 'test1' },
-        { value: '2', label: 'test2' },
-        { value: '3', label: 'test3' }
-    ]
+    const {catalogues, is_catalogues_loading, refresh_catalogues} = useCatalogues();
 
     return (
         <div className="filters-container">
@@ -35,43 +34,47 @@ export function Filters() {
                 <h2 className="filters-header">Фильтры</h2>
                 <button onClick={handleReset} className="reset-button">Сбросить все ×</button>
             </div>
-            <form onSubmit={handleSubmit} className="filters-form">
-                <Select
-                    label="Отрасль"
-                    placeholder="Выберите отрасль"
-                    data={options}
-                    rightSection={<IconChevronDown size="20px" color="var(--grey-500)"/>}
-                    rightSectionWidth={30}
-                    styles={{rightSection: { pointerEvents: 'none' }}}
-                    {...filters_form.getInputProps('catalogue_id')}
-                />
-                <div className="salary-range">
-                    <NumberInput
-                        label="Оклад"
-                        placeholder="От"
-                        step={1000}
-                        min={0}
-                        classNames={{
-                            controlUp: 'salary-input-control up',
-                            controlDown: 'salary-input-control down'
-                        }}
-                        {...filters_form.getInputProps('payment_from')}
+            {!is_catalogues_loading ?
+                <form onSubmit={handleSubmit} className="filters-form">
+                    <Select
+                        label="Отрасль"
+                        placeholder="Выберите отрасль"
+                        data={catalogues}
+                        rightSection={<IconChevronDown size="20px" color="var(--grey-500)"/>}
+                        rightSectionWidth={30}
+                        styles={{rightSection: { pointerEvents: 'none' }}}
+                        {...filters_form.getInputProps('catalogue_id')}
                     />
-                    <NumberInput
-                        placeholder="До"
-                        step={1000}
-                        min={0}
-                        classNames={{
-                            controlUp: 'salary-input-control up',
-                            controlDown: 'salary-input-control down'
-                        }}
-                        {...filters_form.getInputProps('payment_to')}
-                    />
-                </div>
-                <Button type="submit" className="submit-btn">
-                    Применить
-                </Button>
-            </form>
+                    <div className="salary-range">
+                        <NumberInput
+                            label="Оклад"
+                            placeholder="От"
+                            step={1000}
+                            min={0}
+                            classNames={{
+                                controlUp: 'salary-input-control up',
+                                controlDown: 'salary-input-control down'
+                            }}
+                            {...filters_form.getInputProps('payment_from')}
+                        />
+                        <NumberInput
+                            placeholder="До"
+                            step={1000}
+                            min={0}
+                            classNames={{
+                                controlUp: 'salary-input-control up',
+                                controlDown: 'salary-input-control down'
+                            }}
+                            {...filters_form.getInputProps('payment_to')}
+                        />
+                    </div>
+                    <Button type="submit" className="submit-btn">
+                        Применить
+                    </Button>
+                </form>
+                :
+                <div>Loading...</div>
+            }
         </div>
     );
 }
---
+++ client/src/hooks/index.ts
@@ -0,0 +1,1 @@
+export {useCatalogues} from "./useCatalogues";
