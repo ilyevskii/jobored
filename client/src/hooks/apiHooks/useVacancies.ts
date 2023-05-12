1a2683970e12c245dff625b9df7e43ea1df9b372
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-12 05:29:52 +0300

    Changed hook: added tracking of refetching data

--- client/src/hooks/apiHooks/useVacancies.ts
+++ client/src/hooks/apiHooks/useVacancies.ts
@@ -2,20 +2,21 @@ import {useQuery} from "react-query";
 import {SuperjobService, Vacancy} from "services";
 
 import {useRequestParams} from "hooks";
+import {useRef} from "react";
 
 
 export const useVacancies = () => {
 
     const {getActualRequestParams} = useRequestParams();
 
-    const {data, isLoading, isError, error, refetch} = useQuery<Vacancy[] | undefined, Error>(['vacancies'],
+    const {data,  isLoading, isFetching, isError, error, refetch} = useQuery<Vacancy[] | undefined, Error>(['vacancies'],
         async () => {
             return await SuperjobService.getVacancies(getActualRequestParams());
         });
 
     return {
         vacancies: data,
-        isVacanciesLoading: isLoading,
+        isVacanciesLoading: isLoading || isFetching,
         isVacanciesError: isError,
         vacancies_error: error,
         refresh_vacancies: refetch,
