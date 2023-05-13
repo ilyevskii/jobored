4bf8a99c5d9b00573f4cc0baa09fb60ee55d6d5f
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-13 22:12:29 +0300

    Finished creating styles

--- client/src/components/VacanciesList/VacanciesList.tsx
+++ client/src/components/VacanciesList/VacanciesList.tsx
@@ -3,28 +3,45 @@ import React, {useEffect} from 'react';
 
 import {useVacancies} from "hooks";
 import {Vacancy} from "services";
+import {IconStar, IconMapPin} from "@tabler/icons-react";
 
 
 export function VacanciesList() {
 
-    const {vacancies, isVacanciesLoading} = useVacancies();
+    const {vacancies, isVacanciesLoading, refresh_vacancies} = useVacancies();
 
+    const addFavourite = () => {
+
+    }
+
+    useEffect(() => {
+        refresh_vacancies();
+    }, [])
 
     return (
-        <div className="vacancies-container">
+        <ul className="vacancies-list">
             {!isVacanciesLoading ?
                 <>
-                    {vacancies!.length ?
+                    {vacancies && vacancies.length ?
                         <>
                         {vacancies!.map((vacancy: Vacancy) => (
-                            <div className="vacancy" key={vacancy.id}>
+                            <li className="vacancy-item" key={vacancy.id}>
                                 <div className="vacancy-info">
-                                    <p>{vacancy.profession}</p>
-                                    <p>{vacancy.payment_from} {vacancy.currency}</p>
-                                    <p>{vacancy.town}</p>
+                                    <h3 className="vacancy-profession">{vacancy.profession}</h3>
+                                    <p className="vacancy-salary">
+                                        <span>з/п {vacancy.payment_from} {vacancy.currency}</span>
+                                        <span className="vacancy-info-separator">•</span>
+                                        <span>Полный рабочий день</span>
+                                    </p>
+                                    <p className="vacancy-town">
+                                        <img src={`${process.env.PUBLIC_URL}/images/location.png`} alt="location"/>
+                                        <span>{vacancy.town}</span>
+                                    </p>
                                 </div>
-                                <p>star</p>
-                            </div>
+                                <button className="save-btn" onClick={addFavourite}>
+                                    <img src={`${process.env.PUBLIC_URL}/images/star.png`} alt="favourite"/>
+                                </button>
+                            </li>
                         ))}
                         </>
                         :
@@ -37,6 +54,6 @@ export function VacanciesList() {
                     Loading...
                 </div>
             }
-        </div>
+        </ul>
     );
 }
