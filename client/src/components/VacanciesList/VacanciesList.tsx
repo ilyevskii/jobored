4bac114794e9cc4661c450d38d62a14c223566c6
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-14 01:06:56 +0300

    Added the adding/removing favorite vacancies

--- client/src/components/VacanciesList/VacanciesList.tsx
+++ client/src/components/VacanciesList/VacanciesList.tsx
@@ -1,18 +1,23 @@
 import './VacanciesList.css';
 import React, {useEffect} from 'react';
 
-import {useVacancies} from "hooks";
+import {useVacancies, useFavoriteVacancies} from "hooks";
 import {Vacancy} from "services";
-import {IconStar, IconMapPin} from "@tabler/icons-react";
 
 
 export function VacanciesList() {
 
+
+    const {toggleFavoriteVacancy, isFavorite, favorite_vacancies} = useFavoriteVacancies();
     const {vacancies, isVacanciesLoading, refresh_vacancies} = useVacancies();
 
-    const addFavourite = () => {
-
+    const toggleFavourite = (vacancy: Vacancy) => {
+        toggleFavoriteVacancy(vacancy);
     }
+
+    useEffect(() => {
+        console.log(favorite_vacancies)
+    }, [favorite_vacancies])
 
     useEffect(() => {
         refresh_vacancies();
@@ -27,9 +32,9 @@ export function VacanciesList() {
                         {vacancies!.map((vacancy: Vacancy) => (
                             <li className="vacancy-item" key={vacancy.id}>
                                 <div className="vacancy-info">
-                                    <h3 className="vacancy-profession">{vacancy.profession}</h3>
+                                    <h3 className="vacancy-profession bold">{vacancy.profession}</h3>
                                     <p className="vacancy-salary">
-                                        <span>з/п {vacancy.payment_from} {vacancy.currency}</span>
+                                        <span className="bold">з/п {vacancy.payment_from} {vacancy.currency}</span>
                                         <span className="vacancy-info-separator">•</span>
                                         <span>Полный рабочий день</span>
                                     </p>
@@ -38,8 +43,13 @@ export function VacanciesList() {
                                         <span>{vacancy.town}</span>
                                     </p>
                                 </div>
-                                <button className="save-btn" onClick={addFavourite}>
-                                    <img src={`${process.env.PUBLIC_URL}/images/star.png`} alt="favourite"/>
+                                <button className="make-starred-btn" onClick={() => toggleFavourite(vacancy)}>
+                                    {isFavorite(vacancy) ?
+                                        <img src={`${process.env.PUBLIC_URL}/images/star_filled.png`} alt="favourite"/>
+                                        :
+                                        <img src={`${process.env.PUBLIC_URL}/images/star.png`} alt="favourite"/>
+                                    }
+
                                 </button>
                             </li>
                         ))}
