7de51311e05fcf63a19bd683c39a9538f38c6fe5
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-16 02:05:02 +0300

    New component for showing favorite vacancies

---
+++ client/src/components/FavoriteVacanciesList/FavoriteVacanciesList.tsx
@@ -0,0 +1,41 @@
+import './FavoriteVacanciesList.css';
+import React from 'react';
+
+import {useFavoriteVacancies, usePaginationParams} from "hooks";
+import {VacancyItem} from "components";
+import {Vacancy} from "services";
+
+import {Pagination} from "@mantine/core";
+
+
+export function FavoriteVacanciesList() {
+
+    const {favorite_vacancies, getCurrentPageContent} = useFavoriteVacancies();
+    const {current_page, setCurrentPage} = usePaginationParams();
+
+    const vacancies = getCurrentPageContent(current_page);
+
+    return (
+        <>
+            {vacancies.length ?
+                <>
+                    <ul className="vacancies-list favorite">
+                        {vacancies!.map((vacancy: Vacancy) => (
+                            <VacancyItem vacancy={vacancy}/>
+                        ))}
+                    </ul>
+                    <Pagination
+                        className="pagination"
+                        total={Math.ceil(favorite_vacancies.length / 4)}
+                        onChange={setCurrentPage}
+                        value={current_page}
+                    />
+                </>
+                :
+                <div>
+                    No results.
+                </div>
+            }
+        </>
+    );
+}
