edbcb6814aba527d99be3d257413f159e6605f4b
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-16 02:07:36 +0300

    Removed changing of the current menu item by click on menu item

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -6,12 +6,11 @@ import {useMenuItems} from "../../hooks";
 
 export function Header() {
 
-    const {current_menu_item, setCurrentMenuItem} = useMenuItems();
-
+    const {current_menu_item} = useMenuItems();
 
     return (
         <header className="main-header">
-            <div className="logo" onClick={() => setCurrentMenuItem(1)}>
+            <div className="logo">
                 <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="logo"/>
                 <h1 className="logo-text">Jobored</h1>
             </div>
@@ -19,12 +18,10 @@ export function Header() {
                 <ul className="menu-list">
                     <li
                         className={`menu-item${current_menu_item === 1 ? ' active' : ''}`}
-                        onClick={() => setCurrentMenuItem(1)}
-                    ><a>Поиск вакансий</a></li>
+                    ><a href="/">Поиск вакансий</a></li>
                     <li
                         className={`menu-item${current_menu_item === 2 ? ' active' : ''}`}
-                        onClick={() => setCurrentMenuItem(2)}
-                    ><a>Избранное</a></li>
+                    ><a href="/favorites">Избранное</a></li>
                 </ul>
             </nav>
         </header>
