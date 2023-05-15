a41d20b84f0e41f2c98cea7edc15f8cbe563b214
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-15 13:52:17 +0300

    New reducer for saving data connected with pagination

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,3 +1,4 @@
 export {rootReducer, type RootState} from "./reducers/rootReducer";
 
 export {updateCatalogueId, updatePaymentFrom, updatePaymentTo, updateKeywords} from "./reducers/requestParamsReducer";
+export {updateCurrentPage, updateTotalPageAmount} from "./reducers/paginationReducer";
--- client/src/store/reducers/rootReducer.ts
+++ client/src/store/reducers/rootReducer.ts
@@ -2,8 +2,12 @@ import {combineReducers} from 'redux';
 import {Reducer} from "react";
 
 import requestParamsReducer from "./requestParamsReducer";
+import paginationReducer from "./paginationReducer";
+
+
 export const rootReducer: Reducer<any, any> = combineReducers({
-    request_params: requestParamsReducer
+    request_params: requestParamsReducer,
+    pagination: paginationReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
