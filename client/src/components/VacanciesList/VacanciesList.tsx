347576ca9083fd12ebac6c797f6dd490fb6a498d
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-15 13:56:43 +0300

    Added pagination; added loader

--- client/src/components/VacanciesList/VacanciesList.tsx
+++ client/src/components/VacanciesList/VacanciesList.tsx
@@ -1,69 +1,75 @@
 import './VacanciesList.css';
 import React, {useEffect} from 'react';
 
-import {useVacancies, useFavoriteVacancies} from "hooks";
+import {useVacancies, useFavoriteVacancies, usePaginationParams} from "hooks";
 import {Vacancy} from "services";
+
+import {Pagination, Loader} from "@mantine/core";
 
 
 export function VacanciesList() {
 
-
-    const {toggleFavoriteVacancy, isFavorite, favorite_vacancies} = useFavoriteVacancies();
+    const {toggleFavoriteVacancy, isFavorite} = useFavoriteVacancies();
     const {vacancies, isVacanciesLoading, refresh_vacancies} = useVacancies();
+    const {current_page, total_page_amount, setCurrentPage} = usePaginationParams()
 
     const toggleFavourite = (vacancy: Vacancy) => {
         toggleFavoriteVacancy(vacancy);
     }
 
     useEffect(() => {
-        console.log(favorite_vacancies)
-    }, [favorite_vacancies])
-
-    useEffect(() => {
         refresh_vacancies();
-    }, [])
+    }, [current_page])
 
     return (
-        <ul className="vacancies-list">
+        <>
             {!isVacanciesLoading ?
                 <>
                     {vacancies && vacancies.length ?
                         <>
-                        {vacancies!.map((vacancy: Vacancy) => (
-                            <li className="vacancy-item" key={vacancy.id}>
-                                <div className="vacancy-info">
-                                    <h3 className="vacancy-profession bold">{vacancy.profession}</h3>
-                                    <p className="vacancy-salary">
-                                        <span className="bold">з/п {vacancy.payment_from} {vacancy.currency}</span>
-                                        <span className="vacancy-info-separator">•</span>
-                                        <span>Полный рабочий день</span>
-                                    </p>
-                                    <p className="vacancy-town">
-                                        <img src={`${process.env.PUBLIC_URL}/images/location.png`} alt="location"/>
-                                        <span>{vacancy.town}</span>
-                                    </p>
-                                </div>
-                                <button className="make-starred-btn" onClick={() => toggleFavourite(vacancy)}>
-                                    {isFavorite(vacancy) ?
-                                        <img src={`${process.env.PUBLIC_URL}/images/star_filled.png`} alt="favourite"/>
-                                        :
-                                        <img src={`${process.env.PUBLIC_URL}/images/star.png`} alt="favourite"/>
-                                    }
+                            <ul className="vacancies-list">
+                                {vacancies!.map((vacancy: Vacancy) => (
+                                    <li className="vacancy-item" key={vacancy.id}>
+                                        <div className="vacancy-info">
+                                            <h3 className="vacancy-profession bold">{vacancy.profession}</h3>
+                                            <p className="vacancy-salary">
+                                                <span className="bold">з/п {vacancy.payment_from} {vacancy.currency}</span>
+                                                <span className="vacancy-info-separator">•</span>
+                                                <span>Полный рабочий день</span>
+                                            </p>
+                                            <p className="vacancy-town">
+                                                <img src={`${process.env.PUBLIC_URL}/images/location.png`} alt="location"/>
+                                                <span>{vacancy.town}</span>
+                                            </p>
+                                        </div>
+                                        <button className="make-starred-btn" onClick={() => toggleFavourite(vacancy)}>
+                                            {isFavorite(vacancy) ?
+                                                <img src={`${process.env.PUBLIC_URL}/images/star_filled.png`} alt="favourite"/>
+                                                :
+                                                <img src={`${process.env.PUBLIC_URL}/images/star.png`} alt="favourite"/>
+                                            }
 
-                                </button>
-                            </li>
-                        ))}
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                            <Pagination
+                                className="pagination"
+                                total={total_page_amount}
+                                onChange={setCurrentPage}
+                                value={current_page}/>
                         </>
                         :
                         <div>
                             No results.
-                        </div>}
+                        </div>
+                    }
                 </>
                 :
-                <div>
-                    Loading...
+                <div className="loader-container">
+                    <Loader className="loader" size="80px"/>
                 </div>
             }
-        </ul>
+        </>
     );
 }
