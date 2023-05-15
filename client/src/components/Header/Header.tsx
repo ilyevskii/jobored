e6e5bd897434bab3ef845aebf07bfd2b607081ff
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-15 16:32:58 +0300

    Changed some styles; added the showing of current page in header menu

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,21 +1,30 @@
 import './Header.css';
 import React from 'react';
 
+import {useMenuItems} from "../../hooks";
+
+
 export function Header() {
+
+    const {current_menu_item, setCurrentMenuItem} = useMenuItems();
+
 
     return (
         <header className="main-header">
-            <div className="logo">
-                <div className="union">
-                    <div className="logo-ellipse"></div>
-                    <div className="logo-ellipse"></div>
-                </div>
-                <span className="logo-text">Jobored</span>
+            <div className="logo" onClick={() => setCurrentMenuItem(1)}>
+                <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="logo"/>
+                <h1 className="logo-text">Jobored</h1>
             </div>
             <nav id="menu">
-                <ul>
-                    <li><a>Поиск вакансий</a></li>
-                    <li><a>Избранное</a></li>
+                <ul className="menu-list">
+                    <li
+                        className={`menu-item${current_menu_item === 1 ? ' active' : ''}`}
+                        onClick={() => setCurrentMenuItem(1)}
+                    ><a>Поиск вакансий</a></li>
+                    <li
+                        className={`menu-item${current_menu_item === 2 ? ' active' : ''}`}
+                        onClick={() => setCurrentMenuItem(2)}
+                    ><a>Избранное</a></li>
                 </ul>
             </nav>
         </header>
