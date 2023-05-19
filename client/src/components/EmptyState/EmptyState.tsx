7beccea418de1a8b664d2b0ba0b79d0b6d0dc1d7
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-19 18:29:43 +0300

    Added the refreshing of vacancies while clicking fing vacancies button

--- client/src/components/EmptyState/EmptyState.tsx
+++ client/src/components/EmptyState/EmptyState.tsx
@@ -1,14 +1,21 @@
 import './EmptyState.scss';
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {useNavigate} from "react-router-dom";
+import {useVacancies, useVacancyInfo} from "../../hooks";
 
+interface EmptyStateProps {
+    is_error?: boolean;
+}
 
-export function EmptyState() {
+export function EmptyState(props: EmptyStateProps) {
 
     const navigate = useNavigate();
+    const {is_error} = props;
+    const {refresh_vacancies} = useVacancies();
 
     const handleFindVacanciesClick = () => {
+        refresh_vacancies();
         navigate("/");
     }
 
@@ -16,7 +23,11 @@ export function EmptyState() {
     return (
         <main className="empty-state-container">
             <img src={`${process.env.PUBLIC_URL}/images/empty.png`} alt="empty page"/>
-            <p className="alert-text semi-bold">Упс, здесь еще ничего нет!</p>
+            {is_error ?
+                <p className="alert-text semi-bold">Что-то пошло не так... Попробуйте снова!</p>
+                :
+                <p className="alert-text semi-bold">Упс, здесь еще ничего нет!</p>
+            }
             <button className="redirect-btn bold" onClick={handleFindVacanciesClick}>Поиск Вакансий</button>
         </main>
     );
