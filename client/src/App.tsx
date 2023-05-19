49d735f20c8e387d99bff7633b6e06c3a4c610f2
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-19 18:26:47 +0300

    Added new error route, added the handling of incorrect routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
 
 import {EmptyState, Header, VacancyInfo} from "components";
 import {VacanciesPage, FavoriteVacanciesPage} from "pages";
@@ -14,7 +14,9 @@ function App() {
               <Route path="/" element={<VacanciesPage/>}/>
               <Route path="/favorites" element={<FavoriteVacanciesPage/>}/>
               <Route path="/vacancy/:id" element={<VacancyInfo/>}/>
+              <Route path="/error" element={<EmptyState is_error={true}/>}/>
               <Route path="/empty" element={<EmptyState/>}/>
+              <Route path="*" element={<Navigate to="/empty" replace={true}/>}/>
           </Routes>
       </Router>
   );
