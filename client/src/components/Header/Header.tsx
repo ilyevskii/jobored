cb8ae674a23b4714f622f08922626651de7dd3c6
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-21 22:02:39 +0300

    Removed function handleLogoClick

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
-import './Header.scss';
-import React from 'react';
+import "./Header.scss";
+import React from "react";
 
 import {useMenuItems} from "hooks";
 import {useNavigate} from "react-router-dom";
@@ -10,24 +10,20 @@ export function Header() {
     const {current_menu_item} = useMenuItems();
     const navigate = useNavigate();
 
-    const handleLogoClick = () => {
-        navigate("/");
-    }
-
 
     return (
         <header className="site-header">
-            <div className="logo" onClick={handleLogoClick}>
+            <div className="logo" onClick={() => navigate("/vacancies?page=1")}>
                 <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="logo"/>
                 <h1 className="logo-text">Jobored</h1>
             </div>
             <nav id="menu">
                 <ul className="menu-list">
                     <li
-                        className={`menu-item${current_menu_item === 1 ? ' active' : ''}`}
+                        className={`menu-item${current_menu_item === 1 ? " active" : ""}`}
                     ><a href="/">Поиск вакансий</a></li>
                     <li
-                        className={`menu-item${current_menu_item === 2 ? ' active' : ''}`}
+                        className={`menu-item${current_menu_item === 2 ? " active" : ""}`}
                     ><a href="/favorites">Избранное</a></li>
                 </ul>
             </nav>
