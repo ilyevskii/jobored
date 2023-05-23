8a4cdc8cfba92c2edc2ee3a5b83fef621dced8b9
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-23 19:04:48 +0300

    Removed paginationReducer

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,5 +1,4 @@
 export {rootReducer, type RootState} from "./reducers/rootReducer";
 
-export {updateCurrentPage, updateTotalPageAmount} from "./reducers/paginationReducer";
 export {updateCurrentMenuItem} from "./reducers/menuItemsReducer";
 export {updateFavoriteVacancies} from "./reducers/favoriteVacanciesReducer";
--- client/src/store/reducers/rootReducer.ts
+++ client/src/store/reducers/rootReducer.ts
@@ -1,13 +1,11 @@
 import {combineReducers} from 'redux';
 import {Reducer} from "react";
 
-import paginationReducer from "./paginationReducer";
 import menuItemsReducer from "./menuItemsReducer";
 import favoriteVacanciesReducer from "./favoriteVacanciesReducer";
 
 
 export const rootReducer: Reducer<any, any> = combineReducers({
-    pagination: paginationReducer,
     menu_items: menuItemsReducer,
     favorite_vacancies: favoriteVacanciesReducer
 });
