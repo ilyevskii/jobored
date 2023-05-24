2ba5514f65084982523c3bd7bb99edce70395a36
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-24 09:37:18 +0300

    Changed routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,12 +11,10 @@ export default function App() {
       <Router basename="/">
           <Header/>
           <Routes>
-              <Route path="/" element={<Navigate to="/vacancies"/>}/>
+              <Route path="/" element={<Navigate to="vacancies"/>}/>
               <Route path="/vacancies" element={<VacanciesPage/>}/>
               <Route path="/favorites" element={<FavoriteVacanciesPage/>}/>
-              <Route path="/vacancy">
-                  <Route path=":id" element={<SingleVacancyInfo/>}/>
-              </Route>
+              <Route path="/vacancy/:id" element={<SingleVacancyInfo/>}/>
               <Route path="/error" element={<EmptyState is_error={true}/>}/>
               <Route path="/empty" element={<EmptyState/>}/>
               <Route path="*" element={<Navigate to="/empty" replace={true}/>}/>
